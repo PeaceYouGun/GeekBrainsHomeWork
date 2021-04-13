e5caa07aef79097a716f32eb7b2fddfa265c1f22
PeaceYouGun <dr.trinux@mail.ru>
2021-04-13 23:35:42 +0300

    1

--- Java_02/src/TestPackage/Test_01.java
+++ Java_02/src/TestPackage/Test_01.java
@@ -1,2 +1,23 @@
-package TestPackage;public class Test_01 {
+package TestPackage;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        char[] arr = text.toCharArray();
+//        int cur = 0;
+//        char[] newArr = new char[arr.length];
+
+        //введите код сюда
+        for (int i = arr.length - 1; i >= 0; i--) {
+//            newArr[cur] = arr[i];
+//            cur++;
+            System.out.print(arr[i]);
+        }
+//        System.out.println(Arrays.toString(newArr));
+
+    }
 }
